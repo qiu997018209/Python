093f8423cd579325c40560fafe6d90079b976647
qiu997018209 <997018209@qq.com>
2017-08-27 01:11:16 +0800

    Day01_输入输出运算符

---
+++ Python基础学习/Day01_输入输出运算符/test.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+name = raw_input("请输入您的名字:")
+sex = "man"
+print("hello world,name=%s,sex=%s"%(name,sex))
+
+'''
+%取余数
+//取整除
+**幂
+'''
+print("9/4=:%d"%(9/4))
+print("9//4=:%d"%(9//4))
+print("2**4=:%d"%(2**4))
+#注意
+print("重复hello5次%s"%('hello'*5))
+d = 2
+d *=4
+print("2*=4%d"%d)
